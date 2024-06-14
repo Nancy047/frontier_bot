3ce2342468570fcb1bde3726e284e735e667e829
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-06-14 20:05:20 +0530

    updated

--- test.py
+++ test.py
@@ -9,6 +9,10 @@ from langchain_google_vertexai import VertexAI
 from pandasai import Agent
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain_core.prompts import PromptTemplate
+from google.cloud import storage
 
 app = FastAPI()
 
@@ -17,6 +21,41 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "lumen-b-ctl-047-e2aeb24b0ea0.jso
 llm = VertexAI(
     model_name="gemini-1.5-pro", temperature=0.5, max_output_tokens=8192
 )
+
+template = """You are provided with a data containing customer details or
+    case details of customer. Using the provided json, give a concise summary of the data. Try to be concise
+    Highlight the specifics and the numbers.
+    
+    Give me a detailed inference based on this data. 
+
+    {chat_history}
+    df input: {human_input}
+    Response:"""
+    
+prompt = PromptTemplate(
+    input_variables=["chat_history", "human_input"], template=template
+)
+memory = ConversationBufferMemory(memory_key="chat_history")
+
+llm_chain = LLMChain(
+    llm=llm,
+    prompt=prompt,
+    memory=memory,
+)
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client.from_service_account_json('lumen-b-ctl-047-e2aeb24b0ea0.json')
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+
+    blob.download_to_filename(destination_file_name)
+
+    print(f"Blob {source_blob_name} downloaded to {destination_file_name}.")
+
+csv_list = ["may_clearingcodes_dispositions_field_updated 1.csv", "may_dispatches_celonis_updated 1.csv", "may_dispositions_tso_updated 1.csv", "may_workflow_tso.csv", "result.csv"]
+for csv in csv_list:
+    download_blob('frontier-data-csv', csv, csv)
 
 clearingcodes_df = pd.read_csv("may_clearingcodes_dispositions_field_updated 1.csv")
 dispatches_celonis_df = pd.read_csv("may_dispatches_celonis_updated 1.csv")
@@ -33,6 +72,13 @@ pai_agent = Agent(
 df_agent = create_pandas_dataframe_agent(llm, hellox_df, verbose=True)
 pai_agent.clear_memory()
 
+async def data_summary(df_json: str, websocket: WebSocket):
+    memory.clear()
+    print("memory is cleared")
+    response = llm_chain.run(df_json)
+    print("===================")
+    print(f"Response: {response}")
+    return response
 
 def df_to_json(df):
     # Convert DataFrame column names to a more JSON-friendly format (e.g., lowercase with underscores)
@@ -128,10 +174,19 @@ async def pandas_response(query: str):
 @app.websocket("/pd")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+    memory.clear()
     while True:
         data = await websocket.receive_text()
         response = await pandas_response(data)
         print(type(response))
+        if response["table"] != None:
+            # await websocket.send_text("Do you want to summarize?")
+            # data = await websocket.receive_text()
+            # if data.lower() == "yes":
+            print("Summarizing")
+            # summary = await data_summary(response["table"], websocket)
+            summary = await data_summary(json.dumps(response["table"]), websocket)
+            await websocket.send_text(summary)
         try:
             await websocket.send_json(response)
         except:
@@ -141,4 +196,4 @@ async def websocket_endpoint(websocket: WebSocket):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8000)
+    uvicorn.run(app, port=8999)
